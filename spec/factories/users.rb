d5afe8348eb85736840cec477c94ab49b6a945e9
fumi238000 <s21231128@gmail.com>
2021-05-25 11:06:46 +0900

    [add]ユーザーのモデルスペックを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
-# FactoryBot.define do
-#   factory :user do
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    name { "ユーザーネーム" }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 8) }
+  end
+end
