b6f0a0b4661462229202d7a851f8a07e15688837
fumi238000 <s21231128@gmail.com>
2021-05-25 13:58:29 +0900

    [add]materialのテストデータを実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
 # TODO: 定数をまとめること
+# 定数
 USER_NUM = 5
 CONTENT_NUM = 10
+MATERIALS_NUM = 5
 
+#-----------------------------------------
 puts "テストデータのインポート開始"
+#-----------------------------------------
 
 #-----------------------------------------
-# user
+# user sample date
 #-----------------------------------------
-# ユーザーのサンプルデータ
 USER_NUM.times do |i|
   id = i + 1
   User.find_or_create_by!(email: "user#{id}@example.com") do |u|
@@ -19,7 +22,7 @@ end
 puts "ユーザーのテストデータを作成しました"
 
 #-----------------------------------------
-# content
+# content sample date
 #-----------------------------------------
 # コンテンツのサンプルデータ
 CONTENT_NUM.times do |i|
@@ -34,4 +37,24 @@ CONTENT_NUM.times do |i|
 end
 puts "コンテンツのテストデータを作成しました"
 
+#-----------------------------------------
+# material sample date
+#-----------------------------------------
+# 材料のサンプルデータ
+Content.includes(:materials).each do |content|
+  MATERIALS_NUM.times do |i|
+    num = i + 1
+    content.materials.find_or_create_by!(name: "ビス#{num}") do |m|
+      m.content_id = content.id
+      m.name = "ねじ#{num}"
+      m.amount = 100
+      m.unit = "個"
+    end
+  end
+end
+
+puts "材料のテストデータを作成しました"
+
+#-----------------------------------------
 puts "テストデータのインポート終了"
+#-----------------------------------------
