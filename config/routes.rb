e126c2d1a6802cd023dd47e0dd0874b15b87b655
fumi238000 <s21231128@gmail.com>
2021-05-26 01:00:26 +0900

    [add]トップページの機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "homes#index"
+  resources :contents, only: %i[index new edit create update destroy]
+  get "/content_show/:id", to: "contents#show", as: "content_show"
 end
