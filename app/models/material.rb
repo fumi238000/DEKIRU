e2647a83fa176bffeaa76f74552498805f9f7146
fumi238000 <s21231128@gmail.com>
2021-05-25 15:06:02 +0900

    [add]Materialテーブルにバリデージョンを実装

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,3 +1,8 @@
 class Material < ApplicationRecord
   belongs_to :content
+
+  validates :name,   length: { in: 1..16, allow_blank: true }
+  validates :amount, length: { in: 1..5,  allow_blank: true }, numericality: { only_integer: true }
+  # TDOO: enumを使用して選択できる様にするか検討する
+  validates :unit,   length: { in: 1..5,  allow_blank: true }
 end
