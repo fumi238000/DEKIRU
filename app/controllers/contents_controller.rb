1dbd59ec7f9fdd38f1c9c0bb6e93da6e45319080
fumi238000 <s21231128@gmail.com>
2021-05-26 10:27:11 +0900

    [add]conetntのCRUD処理を実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,51 @@
 class ContentsController < ApplicationController
-  def show
-    @content = Content.find(params[:id])
+  # TODO: adminユーザー出ない場合、リダイレクトする処理を実装する
+  before_action :set_content, only: %i[show update edit destroy]
+
+  def index
+    @contents = Content.all
+    # @contents = Content.includes([:materials, :makes])
   end
+
+  def new
+    @content = Content.new
+  end
+
+  def show
+  end
+
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to content_show_path(id: @content.id), notice: "【#{@content.title}】を作成しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @content.update(content_params)
+      redirect_to content_show_path(id: @content.id), notice: "【#{@content.title}】の内容を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @content.destroy!
+    redirect_to contents_path, alert: "【#{@content.title}】を削除しました"
+  end
+
+  private
+
+    def set_content
+      @content = Content.find(params[:id])
+    end
+
+    def content_params
+      params.require(:content).permit(:title, :subtitle, :movie_url, :comment, :point)
+    end
 end
