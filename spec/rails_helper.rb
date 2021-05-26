1b6fcf10b3b7f8754cd6350979f7b8ddd44fbfee
fumi238000 <s21231128@gmail.com>
2021-05-26 15:52:06 +0900

    [add]Rspecでdeviseのヘルパーメソッドが使用できる様に設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods # FactoryBotを省略
   Faker::Config.locale = :ja # 日本語のダミーデータを使用
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
