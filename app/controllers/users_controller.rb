570e6301835425fe9d876d9fee6522d1d6a73b43
fumi238000 <s21231128@gmail.com>
2021-05-27 23:29:08 +0900

    [add]マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def show
+    # @user = User.find_by(current_user)
+  end
+
+  def set_user
+    # ここでログインユーザー出ないとエラーが出る様にする
+    @user = User.find_by(id: current_user)
   end
 end
