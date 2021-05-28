35f2a5d8a2bf294ca7925b13dd9564ef97e34f24
fumi238000 <s21231128@gmail.com>
2021-05-28 12:04:11 +0900

    [add]新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: "不正なアクセスです" and return unless user_signed_in?     # ログインしているか
     redirect_to root_path, alert: "不正なアクセスです" and return unless current_user.admin? # ログインユーザーが管理者か
   end
+
+  # TODO: 入力箇所を検討すること
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # TODO: 入力箇所を検討すること
+  protected
+
+    # 新規登録用ストロングパラメータ
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
