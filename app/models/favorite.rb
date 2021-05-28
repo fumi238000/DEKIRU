638053ceb47908a03de675503259827c5725b13e
fumi238000 <s21231128@gmail.com>
2021-05-28 18:58:26 +0900

    [add]テーブルの紐付け・バリデージョンの追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,9 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :content
+
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: "同じコンテンツをお気に入りすることはできません。",
+  }
 end
