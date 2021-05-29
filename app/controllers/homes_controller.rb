e88939e9436cfbf38293183ce6fdc30735cb4da6
fumi238000 <s21231128@gmail.com>
2021-05-29 18:08:23 +0900

    [update]TOPページのコンテンツ表示を更新

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
+  # TODO: 共通化すること
+  TOP_PAGE_CONTENT = 3 # おすすめコンテンツの最大数
+
   def index
-    # ここに人気・おすすめ・カテゴリー・キーワードを表示する
-    @popularity_contents = Content.first(3) # TODO: 人気ベスト3
-    @recommend_contents = Content.first(3) # TODO: おすすめをランダムで3つ表示
-    @new_contents = Content.order(created_at: :desc).first(3)
+    @popularity_contents = Content.order("RAND()").first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
+    @recommend_contents = Content.recommend.order("RAND()").limit(TOP_PAGE_CONTENT)
+    @new_contents = Content.order(created_at: :desc).first(TOP_PAGE_CONTENT)
   end
 end
