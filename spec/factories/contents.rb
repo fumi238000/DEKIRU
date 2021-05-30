231e49f6b079de8007254f9be294a0e92f0427cd
fumi238000 <s21231128@gmail.com>
2021-05-30 15:30:57 +0900

    [update]content/categoryのテストデータの修正

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :content do
+    # association :category, factory: :category
     title { "コンテンツタイトル" }
     subtitle { "サブタイトル" }
-    movie_url { "URL" }
+    movie_url { "https://www.youtube.com/watch?v=Otrc2zAlJyM" }
     comment { "コメント" }
     point { "ポイント" }
-    # recommend_status { 1 }
+    movie_id { YoutubeUrlFormatter.movie_id_format(movie_url) }
   end
 end
