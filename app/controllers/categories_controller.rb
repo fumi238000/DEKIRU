df390ac1c1e60e1b60282d76e7c2a681d09ee4c2
fumi238000 <s21231128@gmail.com>
2021-05-30 18:55:18 +0900

    [add]categoryのCRUD処理を実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,48 @@
 class CategoriesController < ApplicationController
+  before_action :admin_checker, only: %i[new create update edit destroy]
+  before_action :set_category, only: %i[update edit destroy]
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
-  end
-
-  def destroy
-  end
-
-  def edit
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "カテゴリーを追加しました"
+    else
+      render :new
+    end
   end
 
   def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "カテゴリーを更新しました"
+    else
+      render :edit
+    end
   end
+
+  def edit
+    @content_id = params[:content_id]
+  end
+
+  def destroy
+    @category.destroy!
+    redirect_to categories_path, alert: "カテゴリーを削除しました"
+  end
+
+  private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
