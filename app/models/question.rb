d4a79dfe0dbf7511d094bbad25afe70902506e92
fumi238000 <s21231128@gmail.com>
2021-05-31 01:50:04 +0900

    [add]Responseのテストデータを実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :content
   belongs_to :user
-  has_one :questions, dependent: :destroy
+  has_one :response, dependent: :destroy
 
   validates :question_content, presence: true, length: { in: 1..100, allow_blank: true }
 end
