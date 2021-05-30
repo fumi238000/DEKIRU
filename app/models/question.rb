cbd1dc18cf7467c72ee385d319a969fdda5f0a93
fumi238000 <s21231128@gmail.com>
2021-05-30 23:29:16 +0900

    [add]Questionのモデルスペックを実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :content
   belongs_to :user
+
+  validates :question_content, presence: true, length: { in: 1..100, allow_blank: true }
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :question do
-    content { nil }
-    user { nil }
-    content { "MyString" }
-    status { 1 }
+    association :content, factory: :content
+    association :user, factory: :user
+    question_content { Faker::Lorem.characters(number: 100) }
+    status { 0 }
   end
 end
