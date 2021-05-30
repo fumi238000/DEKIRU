65bdb9b3a61333114bb6fe6c3e20b210205abe63
fumi238000 <s21231128@gmail.com>
2021-05-30 19:28:52 +0900

    [add]トップページにカテゴリーを追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,5 +6,6 @@ class HomesController < ApplicationController
     @popularity_contents = Content.order("RAND()").first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
     @recommend_contents = Content.recommend.order("RAND()").limit(TOP_PAGE_CONTENT)
     @new_contents = Content.order(created_at: :desc).first(TOP_PAGE_CONTENT)
+    @categories = Category.includes(:contents).first(TOP_PAGE_CONTENT)
   end
 end
