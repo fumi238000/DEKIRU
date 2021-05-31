154613066ea7f445cc8e48bf37748c599ee60ffb
fumi238000 <s21231128@gmail.com>
2021-05-31 16:19:13 +0900

    [add]返信の削除機能を追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,8 @@ class HomesController < ApplicationController
   TOP_PAGE_CONTENT = 3 # おすすめコンテンツの最大数
 
   def index
-    @popularity_contents = Content.order("RAND()").first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
+    @popularity_contents = Content.first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
+    # @popularity_contents = Content.order("RAND()").first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
     @recommend_contents = Content.recommend.order("RAND()").limit(TOP_PAGE_CONTENT)
     @new_contents = Content.order(created_at: :desc).first(TOP_PAGE_CONTENT)
     @categories = Category.includes(:contents).first(TOP_PAGE_CONTENT)
