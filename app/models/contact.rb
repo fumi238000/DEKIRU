16ab3226ca1e252435d777abf3755414db836a39
fumi238000 <s21231128@gmail.com>
2021-06-01 11:55:29 +0900

    [add]バリデージョン/enumの実装

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { in: 1..32 }
+  validates :content, presence: true, length: { in: 1..1000 }
+  validates :remote_ip, presence: true
+
+  enum status: { before: 0, after: 1 }
 end
