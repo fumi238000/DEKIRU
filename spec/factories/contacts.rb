a2d41d42027ac56eeec78291cf4760c62e6218e4
fumi238000 <s21231128@gmail.com>
2021-06-01 12:06:17 +0900

    [add]Contactのモデルスペックを実装

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :contact do
-    user { nil }
-    title { "MyString" }
-    content { "MyText" }
-    remote_ip { "MyString" }
-    status { 1 }
+    association :user, factory: :user
+    title { Faker::Lorem.characters(number: 32) }
+    content { Faker::Lorem.characters(number: 1000) }
+    remote_ip { "remote_ip" }
+    status { 0 }
+  end
+
+  trait :contact_invalid do
+    title { "" }
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,64 @@
 require "rails_helper"
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    subject { contact.valid? }
+
+    context "データが条件を満たす時" do
+      let(:contact) { build(:contact) }
+      it "保存ができる" do
+        expect(subject).to eq true
+      end
+    end
+
+    describe "title" do
+      context "入力さていない時" do
+        let(:contact) { build(:contact, title: "") }
+        it "保存ができない" do
+          expect(subject).to eq false
+          expect(contact.errors[:title]).to include "を入力してください"
+        end
+      end
+
+      context "32文字の場合" do
+        let(:contact) { build(:contact, title: "1" * 32) }
+        it "保存ができる" do
+          expect(subject).to eq true
+        end
+      end
+
+      context "33文字の場合" do
+        let(:contact) { build(:contact, title: "1" * 33) }
+        it "保存ができない" do
+          expect(subject).to eq false
+          expect(contact.errors.messages[:title]).to include "は32文字以内で入力してください"
+        end
+      end
+
+      describe "content" do
+        context "入力さていない時" do
+          let(:contact) { build(:contact, content: "") }
+          it "保存ができない" do
+            expect(subject).to eq false
+            expect(contact.errors[:content]).to include "を入力してください"
+          end
+        end
+
+        context "1000文字の場合" do
+          let(:contact) { build(:contact, content: "1" * 1000) }
+          it "保存ができる" do
+            expect(subject).to eq true
+          end
+        end
+
+        context "1001文字の場合" do
+          let(:contact) { build(:contact, content: "1" * 1001) }
+          it "保存ができない" do
+            expect(subject).to eq false
+            expect(contact.errors.messages[:content]).to include "は1000文字以内で入力してください"
+          end
+        end
+      end
+    end
+  end
 end
