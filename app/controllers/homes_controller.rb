4a34ae0ce603d9275ecd88c2a370cbd4916b9a27
fumi238000 <s21231128@gmail.com>
2021-06-02 17:58:17 +0900

    [add]トップページを人気順に表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,7 @@ class HomesController < ApplicationController
   TOP_PAGE_CONTENT = 3 # おすすめコンテンツの最大数
 
   def index
-    @popularity_contents = Content.first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
-    # @popularity_contents = Content.order("RAND()").first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
+    @popularity_contents = Content.order_populer.first(TOP_PAGE_CONTENT) # TODO: 人気ベスト10をランダムに表示
     @recommend_contents = Content.recommend.order("RAND()").limit(TOP_PAGE_CONTENT)
     @new_contents = Content.order(created_at: :desc).first(TOP_PAGE_CONTENT)
     @categories = Category.includes(:contents).first(TOP_PAGE_CONTENT)
