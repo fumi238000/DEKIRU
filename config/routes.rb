50b7a5b319ca855eefe124e394de8478a5e8c4cb
fumi238000 <s21231128@gmail.com>
2021-06-03 18:23:24 +0900

    [add]TagMasterのCRUD処理を実装

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   resources :questions, only: %i[index create destroy]
   resources :responses, only: %i[new create update edit destroy]
   resources :contacts, only: %i[index create]
+  resources :tag_masters, only: %i[new create update edit destroy]
 end
