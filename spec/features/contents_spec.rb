3455aa57d0c030a0907574408af646cdbe72a59d
fumi238000 <s21231128@gmail.com>
2021-06-04 14:52:16 +0900

    [update]featureテストを修正

--- spec/features/contents_spec.rb
+++ spec/features/contents_spec.rb
@@ -19,18 +19,18 @@ RSpec.describe "Contents", type: :feature do
       # 異常値のみ
       it "各リンクが表示されないこと" do
         visit content_show_path(@content.id)
-        expect(page).not_to have_link "追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
-        expect(page).not_to have_link "追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
-        expect(page).not_to have_link "編集する", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
-        expect(page).not_to have_link "削除する", href: material_path(@material.id)
-        expect(page).not_to have_link "編集する", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
-        expect(page).not_to have_link "削除する", href: make_path(@make.id)
-        expect(page).not_to have_link "レビューする >>", href: new_review_path(params: { content_id: @content.id })
-        expect(page).not_to have_link "削除", href: question_path(@do_question)
-        expect(page).not_to have_link "削除", href: question_path(@end_question)
+        expect(page).not_to have_link ">>追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
+        expect(page).not_to have_link ">>追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
+        expect(page).not_to have_link ">>編集", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
+        expect(page).not_to have_link ">>削除", href: material_path(@material.id)
+        expect(page).not_to have_link ">>編集", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
+        expect(page).not_to have_link ">>削除", href: make_path(@make.id)
+        expect(page).not_to have_link ">>レビューする", href: new_review_path(params: { content_id: @content.id })
+        expect(page).not_to have_link ">>削除", href: question_path(@do_question)
+        expect(page).not_to have_link ">>削除", href: question_path(@end_question)
         expect(page).not_to have_link "返信する", href: new_response_path(question_id: @do_question.id)
-        expect(page).not_to have_link "編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
-        expect(page).not_to have_link "削除", href: response_path(@end_question.response)
+        expect(page).not_to have_link ">>編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
+        expect(page).not_to have_link ">>削除", href: response_path(@end_question.response)
       end
     end
 
@@ -39,18 +39,18 @@ RSpec.describe "Contents", type: :feature do
       it "各リンクが表示されないこと" do
         sign_in @user
         visit content_show_path(@content.id)
-        expect(page).not_to have_link "追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
-        expect(page).not_to have_link "追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
-        expect(page).not_to have_link "編集する", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
-        expect(page).not_to have_link "削除する", href: material_path(@material.id)
-        expect(page).not_to have_link "編集する", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
-        expect(page).not_to have_link "削除する", href: make_path(@make.id)
-        expect(page).to have_link "レビューする >>", href: new_review_path(params: { content_id: @content.id })
-        expect(page).to have_link "削除", href: question_path(@do_question)
-        expect(page).to have_link "削除", href: question_path(@end_question)
-        expect(page).not_to have_link "返信する", href: new_response_path(question_id: @do_question.id)
-        expect(page).not_to have_link "編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
-        expect(page).not_to have_link "削除", href: response_path(@end_question.response)
+        expect(page).not_to have_link ">>追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
+        expect(page).not_to have_link ">>追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
+        expect(page).not_to have_link ">>編集", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
+        expect(page).not_to have_link ">>削除", href: material_path(@material.id)
+        expect(page).not_to have_link ">>編集", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
+        expect(page).not_to have_link ">>削除", href: make_path(@make.id)
+        expect(page).to have_link ">>レビューする", href: new_review_path(params: { content_id: @content.id })
+        expect(page).to have_link ">>削除", href: question_path(@do_question)
+        expect(page).to have_link ">>削除", href: question_path(@end_question)
+        expect(page).not_to have_link ">>返信する", href: new_response_path(question_id: @do_question.id)
+        expect(page).not_to have_link ">>編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
+        expect(page).not_to have_link ">>削除", href: response_path(@end_question.response)
       end
     end
 
@@ -58,30 +58,30 @@ RSpec.describe "Contents", type: :feature do
       it "新規作成リンクが表示されること" do
         sign_in @admin
         visit content_show_path(@content.id)
-        expect(page).to have_link "追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
-        expect(page).to have_link "追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
+        expect(page).to have_link ">>追加", href: new_material_path(params: { content_id: @content.id }) # 材料新規作成
+        expect(page).to have_link ">>追加", href: new_make_path(params: { content_id: @content.id }) # 作り方新規作成
       end
 
       context "材料が存在する時" do
         it "リンクが表示される" do
           sign_in @admin
           visit content_show_path(@content.id)
-          expect(page).to have_link "編集する", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
-          expect(page).to have_link "削除する", href: material_path(@material.id)
+          expect(page).to have_link ">>編集", href: edit_material_path(@material.id, params: { content_id: @material.content.id })
+          expect(page).to have_link ">>削除", href: material_path(@material.id)
         end
       end
 
       it "作り方が存在する時、リンクが表示される" do
         sign_in @admin
         visit content_show_path(@content.id)
-        expect(page).to have_link "編集する", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
-        expect(page).to have_link "削除する", href: make_path(@make.id)
+        expect(page).to have_link ">>編集", href: edit_make_path(@make.id, params: { content_id: @make.content.id })
+        expect(page).to have_link ">>削除", href: make_path(@make.id)
       end
 
       it "レビューリンクが表示されないこと" do
         sign_in @admin
         visit content_show_path(@content.id)
-        expect(page).not_to have_link "レビューする >>", href: new_review_path(params: { content_id: @content.id })
+        expect(page).not_to have_link ">>レビューする", href: new_review_path(params: { content_id: @content.id })
       end
 
       it "質問できない状態にあること" do
@@ -96,8 +96,8 @@ RSpec.describe "Contents", type: :feature do
         it "リンクが表示される" do
           sign_in @admin
           visit content_show_path(@content.id)
-          expect(page).to have_link "削除", href: question_path(@do_question)
-          expect(page).to have_link "削除", href: question_path(@end_question)
+          expect(page).to have_link ">>削除", href: question_path(@do_question)
+          expect(page).to have_link ">>削除", href: question_path(@end_question)
         end
       end
 
@@ -105,7 +105,7 @@ RSpec.describe "Contents", type: :feature do
         it "リンクが表示される" do
           sign_in @admin
           visit content_show_path(@content.id)
-          expect(page).to have_link "返信する", href: new_response_path(question_id: @do_question.id)
+          expect(page).to have_link ">>返信する", href: new_response_path(question_id: @do_question.id)
         end
       end
 
@@ -113,8 +113,8 @@ RSpec.describe "Contents", type: :feature do
         it "リンクが表示される" do
           sign_in @admin
           visit content_show_path(@content.id)
-          expect(page).to have_link "編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
-          expect(page).to have_link "削除", href: response_path(@end_question.response)
+          expect(page).to have_link ">>編集", href: edit_response_path(@end_question.response, question_id: @end_question.id)
+          expect(page).to have_link ">>削除", href: response_path(@end_question.response)
         end
       end
     end
