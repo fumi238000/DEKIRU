d5ea58fc27a02cbf16a1327ac4783ac961a571fa
fumi238000 <s21231128@gmail.com>
2021-06-07 13:15:54 +0900

    [fix]ユニークの記述を修正

--- app/models/tag_master.rb
+++ app/models/tag_master.rb
@@ -2,7 +2,7 @@ class TagMaster < ApplicationRecord
   has_many :content_tags, foreign_key: :tag_id, dependent: :destroy, inverse_of: :tag_master
   has_many :contents, through: :content_tags
 
-  validates :tag_name, presence: true, length: { in: 1..10, allow_blank: true }, uniqueness: true
+  validates :tag_name, presence: true, length: { in: 1..10, allow_blank: true }, uniqueness: { case_sensitive: true }
 
   enum tag_type: { content: 0, category: 1 }
 end
