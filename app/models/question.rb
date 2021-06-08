3e11e0b5e479c8e9da5801607ff3d9d31f4446e5
fumi238000 <s21231128@gmail.com>
2021-06-08 21:08:16 +0900

    [add]スキップしていたQuestionのリクエストスペックを実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,16 +8,4 @@ class Question < ApplicationRecord
   def response_nil?
     self.response.nil?
   end
-
-  # エラーメッセージ
-  def self.question_error_message(q)
-    case q.errors.full_messages
-    when ["Question contentを入力してください"]
-      "質問が入力されていません"
-    when ["Question contentは100文字以内で入力してください"]
-      "文字数オーバーです。100文字以内で入力してください。"
-    else
-      "予期せぬエラーが発生しました"
-    end
-  end
 end
