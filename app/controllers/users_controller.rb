9e5648803c93fb77dc3be933436f743ae5a0eedc
fumi238000 <s21231128@gmail.com>
2021-06-08 21:25:53 +0900

    [add]ユーザーのリクエストスペックを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show favorite]
 
   def show
     @content_tags = TagMaster.all
   end
 
   def favorite
-    @favorite_contents = current_user.favorited_contents.page(params[:page]).per(PER_PAGE)
+    @favorite_contents = @user.favorited_contents.page(params[:page]).per(PER_PAGE)
   end
 
   def set_user
-    # ここでログインユーザー出ないとエラーが出る様にする
-    @user = User.find_by(id: current_user)
+    @user = current_user
+    redirect_to root_path, alert: "不正なアクセスです" if @user.nil?
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,70 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user) # ユーザー
+    @admin = FactoryBot.create(:user, user_type: "admin") # 管理者
+  end
+
+  describe "GET /show" do
+    subject { get(mypage_path(user_id)) }
+
+    let(:user_id) { @user.id }
+
+    context "未ログインユーザーの場合" do
+      it "リダイレクトする" do
+        subject
+        expect(response).to have_http_status(:found)
+        expect(flash[:alert]).to eq("不正なアクセスです")
+      end
+    end
+
+    context "一般ユーザーの場合" do
+      it "マイページに遷移する" do
+        sign_in @user
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "管理者の場合" do
+      let(:user_id) { @admin.id }
+      it "マイページに遷移する" do
+        sign_in @admin
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "GET /favorite" do
+    subject { get(favorite_contents_path(user_id)) }
+
+    let(:user_id) { @user.id }
+
+    context "未ログインユーザーの場合" do
+      it "リダイレクトする" do
+        subject
+        expect(response).to have_http_status(:found)
+        expect(flash[:alert]).to eq("不正なアクセスです")
+      end
+    end
+
+    context "一般ユーザーの場合" do
+      it "リクエストが成功する" do
+        sign_in @user
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "管理者の場合" do
+      let(:user_id) { @admin.id }
+      it "リクエストが成功する" do
+        sign_in @admin
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
