4ae259477f7a8ce4fb98f185346ac44aff521db4
fumi238000 <s21231128@gmail.com>
2021-06-13 15:31:05 +0900

    [fix]Userのフィーチャーテストを修正

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -11,14 +11,10 @@ RSpec.describe "Users", type: :feature do
     # MEMO: 未ログインユーザーの場合、遷移できないためテストなし
 
     context "ユーザーが一般ユーザーの場合" do
-      it "管理者専用リンクが表示されないこと" do
+      it "管理者専用リンクが表示されないこと", type: :do do
         sign_in @user
         visit mypage_path(@user)
-        expect(page).not_to have_link ">>追加", href: new_tag_master_path # タグ追加
-        expect(page).not_to have_link ">>編集", href: edit_tag_master_path(@tag.id) # タグ編集
-        expect(page).not_to have_link ">>削除", href: tag_master_path(@tag.id) # タグ削除
-        expect(page).not_to have_link ">>追加", href: new_category_path # カテゴリー追加
-        expect(page).not_to have_link ">>新規作成", href: new_content_path # コンテンツ追加
+        expect(page).not_to have_link "管理者画面へ", href: admin_page_path
       end
     end
 
@@ -26,11 +22,7 @@ RSpec.describe "Users", type: :feature do
       it "管理者専用リンクが表示されること" do
         sign_in @admin
         visit mypage_path(@admin)
-        expect(page).to have_link ">>追加", href: new_tag_master_path # タグ追加
-        expect(page).to have_link ">>編集", href: edit_tag_master_path(@tag.id) # タグ編集
-        expect(page).to have_link ">>削除", href: tag_master_path(@tag.id) # タグ削除
-        expect(page).to have_link ">>追加", href: new_category_path # カテゴリー追加
-        expect(page).to have_link ">>新規作成", href: new_content_path # コンテンツ追加
+        expect(page).to have_link "管理者画面へ", href: admin_page_path
       end
     end
   end
