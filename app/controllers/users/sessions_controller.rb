c879e88d13ba61a3946cf5bd51dec7d404698d1e
fumi238000 <s21231128@gmail.com>
2021-06-14 19:28:09 +0900

    [add]停止ユーザーがログインできない機能を実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :discontinued_user_checker, only: [:create] # rubocop:disable all
 
   # ゲストアカウントでログイン
   def guest_sign_in
@@ -10,6 +11,15 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました。「レビュー」「質問」「お気に入り」「お問い合わせ」が実施できます"
   end
 
+  protected
+
+    # 停止ユーザーはログインできない
+    def discontinued_user_checker
+      @user = User.find_by(email: params[:user][:email].downcase)
+      if @user.discontinued?
+        redirect_to new_user_session_path, alert: "このアカウントではログインすることができません。"
+      end
+    end
   # GET /resource/sign_in
   # def new
   #   super
