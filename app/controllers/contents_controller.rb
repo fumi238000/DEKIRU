de05bb42dcb18b7d470a1e970995dfba6f40e65e
fumi238000 <s21231128@gmail.com>
2021-06-14 15:24:00 +0900

    [add]recommend_statusのステータス変更機能を実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -81,6 +81,7 @@ class ContentsController < ApplicationController
 
     def content_params
       movie_id = YoutubeUrlFormatter.movie_id_format(params[:content][:movie_url])
-      params.require(:content).permit(:title, :subtitle, :movie_url, :comment, :point, :public_status, :category_id, tag_master_ids: []).merge(movie_id: movie_id)
+      params.require(:content).permit(:title, :subtitle, :movie_url, :comment, :point, :public_status, :recommend_status, :category_id,
+                                      tag_master_ids: []).merge(movie_id: movie_id)
     end
 end
