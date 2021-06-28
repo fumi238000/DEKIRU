3c73c56651bd5387cdb03d1ed8de3db5701581d5
fumi238000 <s21231128@gmail.com>
2021-06-28 12:45:13 +0900

    [fix]Contactのテストを修正

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :contact do
     association :user, factory: :user
+    name { Faker::Lorem.characters(number: 8) }
+    email { Faker::Internet.free_email }
     title { Faker::Lorem.characters(number: 32) }
     content { Faker::Lorem.characters(number: 1000) }
     remote_ip { "remote_ip" }
